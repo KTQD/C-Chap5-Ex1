ac3464cce3d2bbd2f3df8eb8be887a36913228d8
Xuan Lam Pham <phamxuanlam@gmail.com>
2024-05-12 16:53:52 +0700

    Update main.c

--- main.c
+++ main.c
@@ -2,17 +2,9 @@
 
 int main() {
     int num;
-    unsigned long long factorial = 1;
-
     printf("Enter a number: ");
-    scanf("%d", &num);
-
-    // Calculate factorial using a loop
-    for (int i = 1; i <= num; ++i) {
-        factorial *= i;
-    }
-
-    printf("Factorial of %d is %llu\n", num, factorial);
+    
+    //Your Code
 
     return 0;
 }
